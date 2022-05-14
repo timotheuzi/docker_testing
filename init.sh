40a346b65f65c1278dae38a565ccf3708b96c417
some guy <22945063+timotheuzi@users.noreply.github.com>
2022-05-14 02:52:23 -0500

    CHANGED TO team red miner for radeon support - also attempting to build radeon docker image

--- init.sh
+++ init.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /config/config.json ]; then
-	cp /home/nobody/config.json /config/config.json
-fi
-
-./t-rex --api-generate-key $API_PASSWORD -c /config/config.json & 
-
-echo Starting T-rex miner...
+echo Starting team red miner...
 echo ============================================================
 echo Server: $SERVER
 echo Algorithm: $ALGO
@@ -15,5 +9,4 @@ echo Worker: $WORKER
 echo Pass: $PASS
 echo ============================================================
 
-
-./t-rex -c /config/config.json -a $ALGO -o $SERVER -u $WALLET -p $PASS -w $WORKER
+./teamredminer -a kawpow -o stratum+tcp://stratum.ravenminer.com:3838 -u RA9w4yJRaUoVXyfHqenu8BWUYwjiJKQ9Uv.trmtest -p x
